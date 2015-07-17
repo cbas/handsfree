bd4f9e4abdb713c3285233cabd462d8eaff5820c
Sebastiaan Deckers <seb@ninja.sg>
2015-07-18 02:19:38 +0800

    Split into lib and app
    Also use dom4 and domready

--- js/app.js
+++ js/app.js
@@ -1,28 +1,15 @@
-export class Handsfree {
-  constructor () {
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-    var recognition = new SpeechRecognition()
-    recognition.continuous = true
-    recognition.interimResults = false
-    recognition.addEventListener('start', event => {
-      console.log('start', event.results)
-    })
-    recognition.addEventListener('result', event => {
-      document.querySelector('textarea').value += `${event.resultIndex}\n`
-      const results = Array.from(event.results)
-        .slice(event.resultIndex)
-        .filter(result => result.final)
-      console.log('results', results)
-    })
-    recognition.addEventListener('error', ({error, message}) => {
-      console.error(error, message)
-    })
-    recognition.addEventListener('end', event => {
-      console.log('end')
-      recognition.start()
-    })
+import dom4 from 'dom4' // eslint-disable-line no-unused-vars
+import domready from 'domready'
+import Handsfree from '../lib'
 
-    document.querySelector('button')
-      .addEventListener('click', event => recognition.start())
-  }
-}
+domready(() => {
+  let handsfree = new Handsfree()
+  document.query('button.start')
+    .addEventListener('click', event => {
+      handsfree.start()
+    })
+  document.query('button.stop')
+    .addEventListener('click', event => {
+      handsfree.stop()
+    })
+})
