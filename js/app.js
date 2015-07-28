5d30c0ba3ac41eedbae7861ebde2f6e80eb8f959
Sebastiaan Deckers <seb@ninja.sg>
2015-07-28 12:32:01 +0800

    Match/Miss events

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
 import dom4 from 'dom4' // eslint-disable-line no-unused-vars
 import domready from 'domready'
+import { on } from 'bubbly'
 import Handsfree from '../lib'
 
 domready(() => {
   let handsfree = new Handsfree()
+  handsfree::on('miss', ::console.log)
+  handsfree::on('match', ::console.info)
+  handsfree::on('match', ({ detail: transcript }) => {
+    console.log({ transcript })
+  })
   document.query('button.start')
-    .addEventListener('click', event => {
+    ::on('click', event => {
       handsfree.start()
     })
   document.query('button.stop')
-    .addEventListener('click', event => {
+    ::on('click', event => {
       handsfree.stop()
     })
-  document.query('button.clear')
-    .addEventListener('click', event => {
-      document.query('textarea').value = ''
-    })
   document.query('button.test')
-    .addEventListener('click', event => {
+    ::on('click', event => {
       window.alert('Success!')
     })
 })
